2a4642bdd21d4ca63ad6c9cbba481205df897e80
RhaniEgghe <36993237+RhaniEgghe@users.noreply.github.com>
2019-03-27 22:27:09 +0100

    Add project files.

---
+++ TestAPI/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAPI
+{
+    class Mainn
+    {
+        public static void Main()
+        {
+            Logic l1 = new Logic();
+            //Task task1 = l1.CreateClient();
+            //Task task1 = l1.GetClient(102);
+            Task task1 = l1.GetClients();
+            //Task task1 = l1.UpdateClient(102);
+            //Task task1 = l1.ArchiveClient(102);
+            //Task task1 = l1.DeleteClient(102);
+            //Task task1 = l1.RestoreClient(102);
+        }
+    }
+}
